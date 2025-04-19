277a31afcffda394e6d0c18cf5023d698710576d
Loic Sharma <sharma.loic@gmail.com>
2025-04-19 16:11:39 -0700

    Implement method call decode

--- dotnet/Butter/FlutterMessageCodecs.cs
+++ dotnet/Butter/FlutterMessageCodecs.cs
@@ -1,24 +1,69 @@
-using System.Buffers;
 using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Text;
 
 namespace Butter;
 
-public interface FlutterMessageCodec<T> {
+public interface FlutterMessageCodec<T>
+{
   byte[] EncodeMessage(T message);
   T DecodeMessage(byte[] message);
 }
 
-public class FlutterStringCodec : FlutterMessageCodec<string> {
+public class FlutterStringCodec : FlutterMessageCodec<string>
+{
   public static readonly FlutterStringCodec Instance = new FlutterStringCodec();
 
-  public byte[] EncodeMessage(string message) {
-    return Encoding.UTF8.GetBytes(message);
+  public byte[] EncodeMessage(string message) => Encoding.UTF8.GetBytes(message);
+
+  public string DecodeMessage(byte[] message) => Encoding.UTF8.GetString(message);
+}
+
+public class FlutterStandardCodec : FlutterMessageCodec<EncodableValue>
+{
+  public static readonly FlutterStandardCodec Instance = new FlutterStandardCodec();
+
+  public byte[] EncodeMessage(EncodableValue message)
+  {
+    throw new NotImplementedException();
+    // var writer = new StandardCodecWriter();
+    // writer.Write(message);
+    // return writer.GetBuffer();
   }
 
-  public string DecodeMessage(byte[] message) {
-    return Encoding.UTF8.GetString(message);
+  public EncodableValue DecodeMessage(byte[] message)
+  {
+    var reader = new StandardCodecReader(message);
+
+    return reader.ReadValue();
+  }
+}
+
+public class FlutterStandardMethodCodec : FlutterMessageCodec<FlutterMethodCall>
+{
+  public static readonly FlutterStandardMethodCodec Instance = new FlutterStandardMethodCodec();
+
+  public byte[] EncodeMessage(FlutterMethodCall message)
+  {
+    throw new NotImplementedException();
+    // var writer = new StandardCodecWriter();
+    // writer.Write(message);
+    // return writer.GetBuffer();
+  }
+
+  public FlutterMethodCall DecodeMessage(byte[] message)
+  {
+    var reader = new StandardCodecReader(message);
+
+    if (!reader.Read())
+    {
+      throw new InvalidOperationException("No more data to read.");
+    }
+
+    var method = reader.GetString();
+    var arguments = reader.ReadValue().GetMap();
+
+    return new FlutterMethodCall(method, arguments);
   }
 }
 
@@ -90,14 +135,14 @@ public ref struct StandardCodecReader
         break;
       case (byte)StandardCodecType.Float64:
         CurrentType = StandardCodecType.Float64;
-        _ReadAlignment(8);
+        ReadAlignment(8);
         ValueSpan = _buffer.Slice(_position, 8);
         _position += 8;
         _size = null;
         break;
       case (byte)StandardCodecType.LargeInt:
       case (byte)StandardCodecType.String:
-        _size = _ReadSize();
+        _size = ReadSize();
 
         CurrentType = StandardCodecType.String;
         ValueSpan = _buffer.Slice(_position, _size.Value);
@@ -110,7 +155,7 @@ public ref struct StandardCodecReader
       case (byte)StandardCodecType.Float64List:
         CurrentType = (StandardCodecType)type;
 
-        _size = _ReadSize();
+        _size = ReadSize();
         var itemSize = CurrentType switch
         {
           StandardCodecType.UInt8List => 1,
@@ -121,7 +166,7 @@ public ref struct StandardCodecReader
 
         if (itemSize > 1)
         {
-          _ReadAlignment(itemSize);
+          ReadAlignment(itemSize);
         }
 
         ValueSpan = _buffer.Slice(_position, _size.Value * itemSize);
@@ -132,7 +177,7 @@ public ref struct StandardCodecReader
       case (byte)StandardCodecType.Map:
         CurrentType = (StandardCodecType)type;
         ValueSpan = ReadOnlySpan<byte>.Empty;
-        _size = _ReadSize();
+        _size = ReadSize();
         break;
 
       default:
@@ -174,48 +219,59 @@ public ref struct StandardCodecReader
 
   public int GetInt32()
   {
-    _VerifyType(StandardCodecType.Int32);
+    VerifyType(StandardCodecType.Int32);
     return BinaryPrimitives.ReadInt32LittleEndian(ValueSpan);
   }
 
   public long GetInt64()
   {
-    _VerifyType(StandardCodecType.Int64);
+    VerifyType(StandardCodecType.Int64);
     return BinaryPrimitives.ReadInt64LittleEndian(ValueSpan);
   }
 
   public double GetFloat64()
   {
-    _VerifyType(StandardCodecType.Float64);
+    VerifyType(StandardCodecType.Float64);
     return BinaryPrimitives.ReadDoubleLittleEndian(ValueSpan);
   }
 
   public string GetString()
   {
-    _VerifyType(StandardCodecType.String);
+    VerifyType(StandardCodecType.String);
     return Encoding.UTF8.GetString(ValueSpan);
   }
 
   public byte GetUInt8ListValue(int index)
   {
-    _VerifyType(StandardCodecType.UInt8List);
+    VerifyType(StandardCodecType.UInt8List);
     return ValueSpan[index];
   }
 
+  public int GetInt32ListValue(int index)
+  {
+    VerifyType(StandardCodecType.Int32List);
+    return BinaryPrimitives.ReadInt32LittleEndian(ValueSpan.Slice(index * 4));
+  }
+
+  public long GetInt64ListValue(int index)
+  {
+    VerifyType(StandardCodecType.Int64List);
+    return BinaryPrimitives.ReadInt64LittleEndian(ValueSpan.Slice(index * 8));
+  }
 
   public float GetFloat32ListValue(int index)
   {
-    _VerifyType(StandardCodecType.Float32List);
+    VerifyType(StandardCodecType.Float32List);
     return BinaryPrimitives.ReadSingleLittleEndian(ValueSpan.Slice(index * 4));
   }
 
   public double GetFloat64ListValue(int index)
   {
-    _VerifyType(StandardCodecType.Float64List);
+    VerifyType(StandardCodecType.Float64List);
     return BinaryPrimitives.ReadDoubleLittleEndian(ValueSpan.Slice(index * 8));
   }
 
-  private void _ReadAlignment(int alignment)
+  private void ReadAlignment(int alignment)
   {
     var mod = _position % alignment;
     if (mod != 0)
@@ -224,7 +280,7 @@ public ref struct StandardCodecReader
     }
   }
 
-  private int _ReadSize()
+  private int ReadSize()
   {
     var sizeSpan = _buffer.Slice(_position);
     var sizeFirstByte = sizeSpan[0];
@@ -245,11 +301,165 @@ public ref struct StandardCodecReader
     }
   }
 
-  private void _VerifyType(StandardCodecType type)
+  private void VerifyType(StandardCodecType type)
   {
     if (CurrentType != type)
     {
       throw new InvalidOperationException($"Invalid current type {CurrentType}, expected: {type}");
     }
+  }
+}
+
+public record FlutterMethodCall(
+  string Method,
+  IReadOnlyDictionary<EncodableValue, EncodableValue> Arguments);
+
+public static class ReadEncodableValueExtensions
+{
+  public static EncodableValue ReadValue(this ref StandardCodecReader reader)
+  {
+    if (!reader.Read())
+    {
+      throw new InvalidOperationException("No more data to read.");
+    }
+
+    return reader.CurrentType switch
+    {
+      StandardCodecType.Null => EncodableValue.Null,
+      StandardCodecType.True => EncodableValue.True,
+      StandardCodecType.False => EncodableValue.False,
+      StandardCodecType.Int32 => new EncodableValue(reader.GetInt32()),
+      StandardCodecType.Int64 => new EncodableValue(reader.GetInt64()),
+      StandardCodecType.Float64 => new EncodableValue(reader.GetFloat64()),
+      StandardCodecType.String => new EncodableValue(reader.GetString()),
+      StandardCodecType.UInt8List => GetUInt8ListValue(reader),
+      StandardCodecType.Int32List => GetInt32ListValue(reader),
+      StandardCodecType.Int64List => GetInt64ListValue(reader),
+      StandardCodecType.Float32List => GetFloat32ListValue(reader),
+      StandardCodecType.Float64List => GetFloat64ListValue(reader),
+      StandardCodecType.List => ReadListValue(ref reader),
+      StandardCodecType.Map => ReadMapValue(ref reader),
+      _ => throw new NotImplementedException($"Unsupported type: {reader.CurrentType}"),
+    };
+  }
+
+  private static EncodableValue GetUInt8ListValue(StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new byte[size];
+    for (int i = 0; i < size; i++) values[i] = reader.GetUInt8ListValue(i);
+    return new EncodableValue(values);
+  }
+
+  private static EncodableValue GetInt32ListValue(StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new int[size];
+    for (int i = 0; i < size; i++) values[i] = reader.GetInt32ListValue(i);
+    return new EncodableValue(values);
+  }
+
+  private static EncodableValue GetInt64ListValue(StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new long[size];
+    for (int i = 0; i < size; i++) values[i] = reader.GetInt64ListValue(i);
+    return new EncodableValue(values);
+  }
+
+  private static EncodableValue GetFloat32ListValue(StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new float[size];
+    for (int i = 0; i < size; i++) values[i] = reader.GetFloat32ListValue(i);
+    return new EncodableValue(values);
+  }
+
+  private static EncodableValue GetFloat64ListValue(StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new double[size];
+    for (int i = 0; i < size; i++) values[i] = reader.GetFloat64ListValue(i);
+    return new EncodableValue(values);
+  }
+
+  private static EncodableValue ReadListValue(ref StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new List<EncodableValue>(size);
+    for (int i = 0; i < size; i++) values.Add(ReadValue(ref reader));
+    return new EncodableValue(values);
+  }
+
+  private static EncodableValue ReadMapValue(ref StandardCodecReader reader)
+  {
+    var size = reader.GetSize();
+    var values = new Dictionary<EncodableValue, EncodableValue>(size);
+    for (int i = 0; i < size; i++)
+    {
+      var key = ReadValue(ref reader);
+      var value = ReadValue(ref reader);
+      values[key] = value;
+    }
+    return new EncodableValue(values);
+  }
+}
+
+public class EncodableValue
+{
+  private object _value;
+
+  public static readonly EncodableValue Null = new EncodableValue(StandardCodecType.Null, 0);
+  public static readonly EncodableValue True = new EncodableValue(StandardCodecType.True, true);
+  public static readonly EncodableValue False = new EncodableValue(StandardCodecType.False, false);
+
+  public EncodableValue(int value) : this(StandardCodecType.Int32, value) { }
+  public EncodableValue(long value) : this(StandardCodecType.Int64, value) { }
+  public EncodableValue(double value) : this(StandardCodecType.Float64, value) { }
+  public EncodableValue(string value) : this(StandardCodecType.String, value) { }
+  public EncodableValue(byte[] value) : this(StandardCodecType.UInt8List, value) { }
+  public EncodableValue(int[] value) : this(StandardCodecType.Int32List, value) { }
+  public EncodableValue(long[] value) : this(StandardCodecType.Int64List, value) { }
+  public EncodableValue(float[] value) : this(StandardCodecType.Float32List, value) { }
+  public EncodableValue(double[] value) : this(StandardCodecType.Float64List, value) { }
+  public EncodableValue(List<EncodableValue> value) : this(StandardCodecType.List, value) { }
+  public EncodableValue(Dictionary<EncodableValue, EncodableValue> value) : this(StandardCodecType.Map, value) { }
+
+  private EncodableValue(StandardCodecType type, object value)
+  {
+    _value = value;
+    Type = type;
+  }
+
+  public StandardCodecType Type { get; private set; }
+
+  public bool GetBool()
+  {
+    return Type switch
+    {
+      StandardCodecType.False => false,
+      StandardCodecType.True => true,
+      _ => throw new InvalidOperationException($"Encodable value is type {Type}, not boolean"),
+    };
+  }
+
+  public int GetInt32() => Get<int>(StandardCodecType.Int32);
+  public long GetInt64() => Get<long>(StandardCodecType.Int64);
+  public double GetFloat64() => Get<double>(StandardCodecType.Float64);
+  public string GetString() => Get<string>(StandardCodecType.String);
+  public byte[] GetUInt8List() => Get<byte[]>(StandardCodecType.UInt8List);
+  public float[] GetFloat32List() => Get<float[]>(StandardCodecType.Float32List);
+  public double[] GetFloat64List() => Get<double[]>(StandardCodecType.Float64List);
+  public IReadOnlyList<EncodableValue> GetList() => Get<List<EncodableValue>>(StandardCodecType.List);
+  public IReadOnlyDictionary<EncodableValue, EncodableValue> GetMap() => Get<Dictionary<EncodableValue, EncodableValue>>(StandardCodecType.Map);
+
+  private T Get<T>(StandardCodecType type)
+  {
+    if (Type != type)
+    {
+      throw new InvalidOperationException($"Encodable value is type {Type}, not type {type}");
+    }
+
+    return (T)_value;
   }
 }
